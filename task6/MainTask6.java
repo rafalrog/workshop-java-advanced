c0f0191506e8af42145ec1d72b7e2955fca61f5f
Uzytkownik <uzytkownik@kurs.GIT.plgit config --global user.email uzytkownik@kurs.GIT.pl>
2022-02-23 11:45:18 +0100

    task 6

---
+++ task6/MainTask6.java
@@ -0,0 +1,17 @@
+package task6;
+
+public class MainTask6 {
+    public static void main(String[] args) {
+        Movable movablePoint = new MovablePoint(20, 20, 6, 6);
+        System.out.println(movablePoint);
+        movablePoint.moveRight();
+        movablePoint.moveUp();
+        System.out.println(movablePoint);
+        Movable movableCircle = new MovablePoint(40, 60, 8, 8);
+        System.out.println(movableCircle);
+        movableCircle.moveRight();
+        movableCircle.moveUp();
+        System.out.println(movableCircle);
+    }
+}
+
