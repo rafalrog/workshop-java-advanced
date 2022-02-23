3d342964eb9febe7e60b6ff343d7a2a9b7f9605b
Uzytkownik <uzytkownik@kurs.GIT.plgit config --global user.email uzytkownik@kurs.GIT.pl>
2022-02-23 10:54:10 +0100

    task 1

---
+++ task1/MainTask1.java
@@ -0,0 +1,16 @@
+package task1;
+
+public class MainTask1 {
+    public static void main(String[] args) {
+        Point2D emptyPoint2D = new Point2D();
+        System.out.println(emptyPoint2D.toString());
+
+        Point2D point2D = new Point2D(1f, 10f);
+        System.out.println(point2D);
+        point2D.setXY(2f, 20f);
+        System.out.println(point2D);
+        System.out.println(point2D.getXY());
+
+
+    }
+}
---
+++ task1/Point3D.java
@@ -0,0 +1,34 @@
+package task1;
+
+import java.util.ArrayList;
+
+public class Point3D extends Point2D {
+
+    private Float z;
+
+    public Point3D(Float x, Float y, Float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public ArrayList<Float> getXYZ() {
+        ArrayList<Float> arrayList = new ArrayList<Float>();
+        arrayList.add(super.getX());
+        arrayList.add(super.getY());
+        arrayList.add(z);
+        return arrayList;
+    }
+
+    public void setZ(Float z) {
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + super.getX() + "," + super.getY() + "," + z + ")";
+    }
+}
